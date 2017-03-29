8ea373f9f312288e1e31148f97f9fb97b140ae30
rnoc-ne <akrapongj@hotmail.com>
2017-03-29 13:39:41 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'S4hSzyNalrSO4e2T3sPi92ka90zYlUDgYNM2ymlipDK';
+$access_token = 'lmDflC804HfDMESt7elcodTAwK5NqBsxKS7DTl9YhZ7aHxS09g+UJB1dPxm5i45p+2wYgUFahKYhC2bgveZvq1GvxloXl3IgGNZCRnjSSSLail7bDPZsXJ3sF1IXdxcaK04MRK5lM0SCP4GRu9zxfgdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
