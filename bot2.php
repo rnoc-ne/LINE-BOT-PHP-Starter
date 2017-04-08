75a50f1cb516d889514f0fef4c1db0a317d3dd20
rnoc-ne <akrapongj@hotmail.com>
2017-04-08 12:49:11 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -1,4 +1,2 @@
 <?php
-$access_token = 'lmDflC804HfDMESt7elcodTAwK5NqBsxKS7DTl9YhZ7aHxS09g+UJB1dPxm5i45p+2wYgUFahKYhC2bgveZvq1GvxloXl3IgGNZCRnjSSSLail7bDPZsXJ3sF1IXdxcaK04MRK5lM0SCP4GRu9zxfgdB04t89/1O/w1cDnyilFU=';
-
 echo "I am a bot 2  for test";
